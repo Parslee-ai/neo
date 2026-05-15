eca38e744f529a4bd463faba49c6842c77ef274b
Matt Liotta <mliotta@r337.com>
2026-05-15 14:48:46 -0400

    chore: bump version to 0.17.0 (#91)

--- src/neo/__init__.py
+++ src/neo/__init__.py
@@ -2,6 +2,6 @@
 
 from neo.cli import CodeSuggestion, PlanStep, SimulationTrace, StaticCheckResult
 
-__version__ = "0.16.1"
+__version__ = "0.17.0"
 
 __all__ = ["__version__", "CodeSuggestion", "PlanStep", "SimulationTrace", "StaticCheckResult"]
